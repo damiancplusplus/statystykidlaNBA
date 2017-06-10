0b2d90f5d534b8b9586e48b0111d92cc07cf44b7
Damian Czarnowski <Damian Czarnowski>
2017-06-10 14:34:53 +0200

    porzadek katalogow

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "menu.cpp"
+#include "sort.cpp"
+#include "stoper.cpp"
+#include "menu.hpp"
+#include "sort.hpp"
+#include "stoper.hpp"
+
+int main ()
+    {
+        menu();
+        return 0;
+    }
